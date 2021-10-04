465fd43a58e9f815e44085f55137238c1e42f9af
Fraser Thomson <fraserjiawei1@gmail.com>
2021-10-04 22:46:04 +0100

    changed aethetics of search page

--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -11,6 +11,12 @@ class _SearchPageState extends State<SearchPage> {
   List _displayedItems = <String>[];
   List _likedItems = <String>[];
 
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   void initState() {
     super.initState();
     _storage.readItems().then((List wps) {
@@ -27,7 +33,7 @@ class _SearchPageState extends State<SearchPage> {
         results.add(i);
       }
     }
-    return results;
+    return item == "" ? [] : results;
   }
 
   List<Widget> _buildList() {
@@ -43,31 +49,52 @@ class _SearchPageState extends State<SearchPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Search Page"), centerTitle: true),
+      appBar: AppBar(title: Text("Search"), centerTitle: true),
       body: Column(
         children: [
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _displayedItems = _searchItem(_textController.text);
-                      });
-                    },
-                    child: Text("Search"),
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
-                  )),
-              Expanded(
-                  child: TextField(
-                controller: _textController,
-                decoration: InputDecoration(
-                    hintText: "Input liked item to search for..."),
-              ))
-            ],
-          ),
-          Expanded(child: ListView(children: _buildList()))
+          Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 5.0, top: 5.0),
+                      child: Container(
+                          height: 40.0,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _displayedItems =
+                                    _searchItem(_textController.text);
+                              });
+                            },
+                            child: Text("Search"),
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.blue),
+                          ))),
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.only(right: 5.0, top: 5.0),
+                          child: Container(
+                              height: 40.0,
+                              child: TextField(
+                                controller: _textController,
+                                decoration: InputDecoration(
+                                    fillColor: Colors.grey[300],
+                                    filled: true,
+                                    hintText: "Search from liked items..."),
+                              ))))
+                ],
+              )),
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: _displayedItems.length == 0
+                      ? Center(
+                          child: Text(
+                          "0 results",
+                          style: TextStyle(color: Colors.grey),
+                        ))
+                      : ListView(children: _buildList())))
         ],
       ),
     );
